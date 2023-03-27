03fe97a733124dd292a5835d2631387f8113697e
gerardo késsler <gera.kessler@gmail.com>
2023-03-27 11:29:40 -0300

    funcionando con telegram y unigram

---
+++ addon/appModules/telegram.py
@@ -0,0 +1,6 @@
+﻿# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Gerardo Kessler <ReaperYOtrasYerbas@gmail.com>
+# This file is covered by the GNU General Public License.
+
+from . import unigram
+AppModule= unigram.AppModule
--- buildVars.py
+++ buildVars.py
@@ -50,7 +50,7 @@ addon_info = {
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/appModules/telegram/__init__.py", "addon/GlobalPlugins/umStart.py"]
+pythonSources = ["addon/appModules/telegram.py", "addon/appModules/unigram.py", "addon/GlobalPlugins/umStart.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
