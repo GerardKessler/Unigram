fcd8197c668539d85d5cf57f0ff972bb7dac69da
gerardo késsler <gera.kessler@gmail.com>
2023-03-27 11:16:30 -0300

    cambiado el nombre del módulo a telegram

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("Módulo que agrega enfoques automáticos y atajos de teclado a funciones importantes de la aplicación"),
 	# version
-	"addon_version": "1.9",
+	"addon_version": "2.0",
 	# Author(s)
 	"addon_author": "Gerardo Kessler <ReaperYOtrasYerbas@gmail.com>",
 	# URL for the add-on documentation support
@@ -50,7 +50,7 @@ addon_info = {
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/appModules/unigram/__init__.py", "addon/GlobalPlugins/umStart.py"]
+pythonSources = ["addon/appModules/telegram/__init__.py", "addon/GlobalPlugins/umStart.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
